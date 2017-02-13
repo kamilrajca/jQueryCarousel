7a0e372019c00fe993463a34f764ab0111b0f92a
Kamil <kamil.rajca@gmail.com>
2017-02-13 21:00:24 +0100

    poprawienie wciec w js

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,15 @@ $(function(){
   setInterval(changeSlide, 3000); //co 3 sek. wykona funkcję zmieniającą slajd. 
   
   function changeSlide(){
-  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);    
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);    
   }
 
   function moveFirstSlide(){ 
-  var firstItem = carouselList.find("li:first");
-  var lastItem = carouselList.find("li:last");
-  lastItem.after(firstItem);
-  carouselList.css({marginLeft:0});
-}
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  }
 });
 
 
